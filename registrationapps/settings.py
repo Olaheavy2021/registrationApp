de1e8afc18267d1cfeec5ca73db2a19138637b8f
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2023-07-06 11:53:55 +0100

    Configure .env file for the Project

--- registrationapps/settings.py
+++ registrationapps/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -156,6 +159,6 @@ LOGIN_REDIRECT_URL = "studentregistration:home"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sunkanmiolawuwo.gas@gmail.com'
-EMAIL_HOST_PASSWORD = 'ysfwkdhlrzazpuue'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
