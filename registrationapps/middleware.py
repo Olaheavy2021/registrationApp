dada7e077112e2d26be2da55efebb206da5e8950
themaleem <zizo9585@gmail.com>
2023-07-09 11:48:06 +0100

    redirect authenticted user if they visit login/register pages

---
+++ registrationapps/middleware.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.shortcuts import redirect
+
+
+class RedirectLoggedInMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Check if the user is authenticated and if the current path is login or register
+        if request.user.is_authenticated and request.path in [
+            reverse("login"),
+            reverse("register"),
+        ]:
+            # Redirect the user to the dashboard
+            return redirect("dashboard")
+
+        response = self.get_response(request)
+        return response
