493b6db287f571f8b69cce0df2f22e29775b5213
themaleem <zizo9585@gmail.com>
2023-08-07 17:54:49 +0100

    update secret keys

--- registrationapps/settings.py
+++ registrationapps/settings.py
@@ -25,9 +25,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY", "b$(j-$z_y3nlw*%y=7grynrl&nner-%!+&ydw!o!hf@s@wl$!e"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
